d50fec7e4c16509e206554469c03bcd9ec6764a1
Fortune Ndlovu <109510679+Fortune-Ndlovu@users.noreply.github.com>
2023-07-26 04:55:59 +0000

    The main function declares a variable called authors, which is a map of string keys and author values. A map is a data structure that stores key-value pairs, where each key is unique and can be used to retrieve the corresponding value. The next line initializes the map with the make function, which allocates and initializes a map with a given type. In this case, the type is map[string]author, which means the keys are strings and the values are authors. The next two lines add two authors to the map, using the syntax map[key] = value. In this case, the keys are "tm" and "ma", and the values are author structs with the name field set to "Toni Mori" and "Marc Lori", respectively. The next line prints the map using the fmt.Printf function, which prints its arguments to the standard output, formatted according to a format specifier. In this case, the format specifier is %#v, which means to print the value in Go syntax. The output shows the map enclosed in curly braces, with each key-value pair separated by a comma. The last line reads a value from the map using a known key, using the syntax map[key]. In this case, the key is "tm", and the value is the author struct with the name field set to "Toni Mori". The value can be assigned to a variable or used in an expression. The output of this program is:
    map[string]author{"ma":author{name:"Marc Lori"}, "tm":author{name:"Toni Mori"}}

--- types/maps/initialization/begin/main.go
+++ types/maps/initialization/begin/main.go
@@ -1,22 +1,25 @@
 // types/maps/initialization/begin/main.go
 package main
 
+import "fmt"
+
 type author struct {
 	name string
 }
 
 func main() {
 	// declare a map of string keys and author values
-	//
+	var authors map[string]author
 
 	// initialize the map with make
-	//
+	authors = make(map[string]author)
 
 	// add authors to the map
-	//
+	authors["tm"] = author{name: "Toni Mori"}
+	authors["ma"] = author{name: "Marc Lori"}
 
 	// print the map with fmt.Printf
-	//
+	fmt.Printf("%#v\n", authors)
 
 	// read a value from the map with a known key
 	//
